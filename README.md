0986f4e7169f76288df1f0f2e834eb18f41080be
Nicholas West <nickwe@openmetal.io>
2024-07-29 12:56:32 -0400

    Create rbd-export.sh

---
+++ README.md
@@ -0,0 +1,30 @@
+# RBD Sync
+Migrates or synchronizes RBD images, including snapshots, between two Ceph clusters for a specific pool.
+- `--export`: Snapshots source image and exports all snapshots of image into destination.
+- `--sync`: Transfers only the differences between the source and destination images.
+
+Only boot a VM with the migrated volume after performing a sync of the RBD image data, otherwise the filesystem may be corrupt resulting in a VM that does not boot.
+
+This was created to aid in performing an OpenStack migration of VMs from a source cloud to a destination cloud. Data is streamed using `rbd` over SSH. This tool allows an operator to perform an initial copy of the RBD image data to effectively stage the destination cloud, and then perform a sync of the image data some time later. If you're able to use Ceph RBD mirroring, that is probably a better option.
+ 
+Use at your own risk!
+
+## Requirements
+- Root access to remote Ceph cluster over SSH
+- Pools of the same name already created in both Ceph clusters
+
+## Usage
+Before using this tool, ensure you:
+- Create a volume in destination cluster that is the same size as the source image
+- Take note of the RBD image name associated with the volume created in the destination cluster
+- Delete the RBD image in the destination cloud associated with the volume created in the first step
+
+You will need the RBD source image name, the destination RBD image name, and the pool name from which the image is being migrated.
+
+```sh
+# Export and import an image
+./rbd-export.sh --export <source_volume_name> <destination_volume_name> <pool> <remote_host>
+
+# Sync differences of an image
+./rbd-export.sh --sync <source_volume_name> <destination_volume_name> <pool> <remote_host>
+```
---
+++ rbd-sync.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+# Description:
+#   Migrates or synchronizes RBD images between two Ceph clusters.
+#   It supports two main operations:
+#     - Export: Creates a snapshot of the source image on the destination cluster.
+#     - Sync: Transfers only the differences between the source and destination images.
+#
+# Prerequisites:
+#   Destination image created with Cinder of same size as original
+#   The RBD image associated with the destination image must be deleted
+
+help_message="
+RBD Image Migration and Synchronization Script
+
+Usage:
+  $0 --flag <source_image_name> <destination_image_name> <pool> <remote_host>
+
+--export:  Performs a full export of the source image to the destination.
+--sync:    Synchronizes the changes (using snapshots) between the source and destination.
+
+Prerequisites:
+  Destination image created with Cinder of same size as original
+  The RBD image associated with the destination image must be deleted
+"
+
+show_help() {
+    echo "$help_message"
+}
+
+set -e
+
+# process flags
+case "$1" in
+  --export)
+    export_image=true
+    shift
+    ;;
+  --sync)
+    sync_image=true
+    shift
+    ;;
+  --help)
+    show_help
+    exit 0
+    ;;
+  *)
+    echo "Invalid flag: $1" >&2
+    show_help
+    exit 1
+    ;;
+esac
+
+image=$1 
+dest_image=$2 
+pool=$3
+remote=$4
+current_snapshot=snap-1
+sync_snapshot=snap-2
+
+function export_rbd_image() {
+  echo "Exporting RBD image: $image"
+  echo "Creating snapshot: $image@$current_snapshot"
+  rbd snap create -p "$pool" "$image@$current_snapshot"
+
+  # initial image migration
+  snapshots=$(rbd -p "$pool" snap ls "$image" | awk 'NR > 1 {print $2}')
+  if [[ -n $snapshots ]]; then
+    first_snapshot=$(echo $snapshots | awk '{print $1}')
+    echo "Exporting snapshot: $image@$first_snapshot"
+    rbd -p "$pool" export "$image"@"$first_snapshot" - | \
+      pigz -c --fast | \
+      ssh root@"$remote" "pigz -cd | rbd --dest-pool '$pool' import - '$dest_image'"
+
+    echo "Creating snapshot on $remote: $dest_image@$first_snapshot"
+    ssh root@"$remote" "rbd -p '$pool' snap create '$dest_image'@'$first_snapshot'"
+
+    # import remaining snapshots
+    current=$first_snapshot
+    remaining_snapshots=$(echo $snapshots | awk '{$1=""; print $0}')
+    if [[ -n $remaining_snapshots ]]; then
+      for snap in $remaining_snapshots; do
+        echo "Exporting snapshot: $image@$current"
+        rbd -p "$pool" export-diff --from-snap "$current" "$image"@"$snap" - | \
+          pigz -c --fast | \
+          ssh root@"$remote" "pigz -cd | rbd -p '$pool' import-diff - '$dest_image'" 
+        current=$snap
+      done
+    fi
+  fi
+}
+
+function sync_rbd_diffs() {
+  echo "Syncing RBD image differences"
+  echo "Creating snapshot: $image@$sync_snapshot"
+  rbd snap create -p "$pool" "$image@$sync_snapshot"
+
+  # Create new snapshot to capture differences and import
+  rbd -p "$pool" export-diff --from-snap "$current_snapshot" "$image"@"$sync_snapshot" - | \
+    pigz -c --fast | \
+    ssh root@"$remote" "pigz -cd | rbd -p '$pool' import-diff - '$dest_image'"
+}
+
+if [[ "$export_image" == "true" ]]; then
+  export_rbd_image
+fi
+
+if [[ "$sync_image" == "true" ]]; then
+  sync_rbd_diffs
+fi
